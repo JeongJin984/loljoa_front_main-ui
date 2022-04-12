6760ed88065f902d047ff31587f4ba31675a634a
jeongjin984 <philipwq3963@gmail.com>
2022-04-12 23:08:31 +0900

    m

--- src/saga/league-saga.js
+++ src/saga/league-saga.js
@@ -2,7 +2,7 @@ import {all, fork} from 'redux-saga/effects';
 import createSaga from "../utils/createSaga";
 import {backURL} from "../../config/config";
 
-const watchTest = createSaga("TEST", backURL + "/init/db/", "get");
+const watchTest = createSaga("TEST", "http://ae37ffc833def4e25ab5f3a0f433c114-905272317.ap-northeast-2.elb.amazonaws.com/init/db/", "get");
 
 export default function* leagueSaga() {
   yield all([
