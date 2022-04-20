c02102ea43006d58565c40da45f915fa04dc69df
jeongjin984 <philipwq3963@gmail.com>
2022-04-20 10:13:15 +0900

    m

--- pages/login.js
+++ pages/login.js
@@ -7,55 +7,57 @@ import { LOGIN_REQUEST } from "../config/event/eventName/userEvent";
 import { useCookies } from "react-cookie";
 
 const Login = () => {
-    const router = useRouter()
-    const dispatch = useDispatch();
+  const router = useRouter()
+  const dispatch = useDispatch();
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [cookies, setCookie] = useCookies(['user']);
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [cookies, setCookie] = useCookies(['user']);
 
-    const { user } = useSelector(state => state.userReducer)
+  const { user } = useSelector(state => state.userReducer)
 
 
-    useEffect(() => {
+  useEffect(() => {
+    if (user) {
+      router.push("/")
+        .then(
+          () => setCookie("SUID", user.username, { path: "/" })
+        )
+    }
+  }, [user])
 
-        if (user) {
-            setCookie("SUID", user.username, { path: "/" })
-        }
-    }, [user])
+  const onChangeUsername = useCallback((e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value)
+  }, [])
 
-    const onChangeUsername = useCallback((e) => {
-        console.log(e.target.value)
-        setUsername(e.target.value)
-    }, [])
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value)
+  }, [])
 
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value)
-    }, [])
+  const onClickLogin = useCallback(() => {
+    dispatch({
+      type: LOGIN_REQUEST,
+      params: {
+        username,
+        password
+      }
+    })
+  }, [username, password])
 
-    const onClickLogin = useCallback(() => {
-        dispatch({
-            type: LOGIN_REQUEST,
-            params: {
-                username,
-                password
-            }
-        })
-    }, [username, password])
-
-    return (
-        <div style={{ padding: "100px 0", textAlign: "center" }}>
-            <Form>
-                <Form.Field inline>
-                    <input placeholder="ID" onChange={onChangeUsername} value={username} />
-                </Form.Field>
-                <Form.Field inline>
-                    <input type="password" placeholder="Password" onChange={onChangePassword} value={password} />
-                </Form.Field>
-                <Button onClick={onClickLogin} href={"/"}>Login</Button>
-            </Form>
-        </div>
-    )
+  return (
+    <div style={{ padding: "100px 0", textAlign: "center" }}>
+      <Form>
+        <Form.Field inline>
+          <input placeholder="ID" onChange={onChangeUsername} value={username} />
+        </Form.Field>
+        <Form.Field inline>
+          <input type="password" placeholder="Password" onChange={onChangePassword} value={password} />
+        </Form.Field>
+        <Button onClick={onClickLogin} href={"/"}>Login</Button>
+      </Form>
+    </div>
+  )
 
 }
 
