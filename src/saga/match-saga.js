6ab62a4a3d5ff56fe69ab4c58beb2f1957497438
laranja14 <laranja1419@gmail.com>
2022-04-27 10:07:05 +0900

    m

--- src/saga/match-saga.js
+++ src/saga/match-saga.js
@@ -2,7 +2,7 @@ import { all, fork } from 'redux-saga/effects';
 import createSaga from "../utils/createSaga";
 
 const watchMatch = createSaga("CALL_MATCH", "http://aefb1dc6a974c4ec69a3d5e7caa0b442-1183511394.ap-northeast-2.elb.amazonaws.com/api/league/schedule/all", "get");
-const watchGetGameData = createSaga("GET_GAME_DATA", "http://aefb1dc6a974c4ec69a3d5e7caa0b442-1183511394.ap-northeast-2.elb.amazonaws.com/api/betting/game/data", "get" )
+const watchGetGameData = createSaga("GET_GAME_DATA", "http://aefb1dc6a974c4ec69a3d5e7caa0b442-1183511394.ap-northeast-2.elb.amazonaws.com/api/betting/game/data", "get")
 const watchBetting = createSaga("BETTING", "http://aefb1dc6a974c4ec69a3d5e7caa0b442-1183511394.ap-northeast-2.elb.amazonaws.com/api/betting/game/betting", "get")
 const watchAddTeam = createSaga("ADD_TEAM_DATA", "http://localhost:4321/team", "get")
 
